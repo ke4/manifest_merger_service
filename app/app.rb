32118c013c5cadc1971bf90937d36a8c04152a97
Karel <ke4@sanger.ac.uk>
2014-03-25 09:36:59 +0000

    [#67992430] fixed merged manifest sending as an attachment

--- app/app.rb
+++ app/app.rb
@@ -6,13 +6,16 @@ post '/merge_excel/' do
   samples_csv  = params['manifest-details'][:tempfile]
   manifest     = ExcelMerge.merge_template_with_csv(xls_template, samples_csv)
 
-  manifest.write(manifest.io)
-
+  manifest.write "manifest.xls"
   # Use these headers when running as localhost.  Not needed behind nginx though...
   # response.headers['content_type'] = "application/vnd.ms-excel"
   # response.headers['Access-Control-Allow-Origin'] = '*'
   # response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,GET,HEAD,POST,PUT,DELETE,TRACE,CONNECT'
+  # response.headers['Access-Control-Request-Method'] = '*'
+  # response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 
-  send_file(manifest.io)
+   send_file("manifest.xls", :type => "application/vnd.ms-excel", :filename => "manifest.xls", :stream => false)
+
+   File.delete("./manifest.xls")
 end
 
